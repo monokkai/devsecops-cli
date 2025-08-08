e8d061bc76ab53fc45a683a6ff883f64709e0890
monokkai <egormayer08@mail.ru>
2025-08-08 15:44:53 +0200

    dev: doing auth.rs file

--- src/commands/auth.rs
+++ src/commands/auth.rs
@@ -12,6 +12,37 @@ pub fn handle(args: super::cli::AuthAgrs) {
                 &DecodingKey::from_secret(b"secret"),
                 &Validation::new(Algorithm::HS256),
             );
+
+            match decoded {
+                Ok(token_data) => {
+                    println!("{}\n{}", "✅ Valid JWT:".green(), json!(token_data.claims))
+                }
+                Err(e) => eprintln!("{}: {}", "❌ Invalid JWT".red(), e),
+            }
+        }
+
+        AuthAction::Github { token } => {
+            println!("🌐 Verifying GitHub token...");
+
+            let client = Client::new();
+            let response = client
+                .get("https://api.github.com/user")
+                .header("Authorization", format!("Bearer {}", token))
+                .header("User-Agent", "DevSecOps-CLI")
+                .send();
+
+            match response {
+                Ok(res) => {
+                    if res.status().is_success() {
+                        println!("{}", "✅ Valid GitHub token".green());
+                        println!("User: {:?}", res.json::<serde_json::Value>().unwrap());
+                    } else {
+                        eprintln!("{}: {}", "❌ Invalid token".red(), res.status());
+                    }
+                }
+
+                Err(e) => eprintln!("{}: {}", "❌ Request failed".red(), e),
+            }
         }
     }
 }
