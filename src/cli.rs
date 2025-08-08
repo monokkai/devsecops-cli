482bb3dded30d55207aa0642c284a0852ef651c3
monokkai <egormayer08@mail.ru>
2025-08-09 01:37:54 +0200

    dev: some code

--- src/cli.rs
+++ src/cli.rs
@@ -1,4 +1,4 @@
-use clap::{Parser, Subcommand};
+use clap::{Args, Parser, Subcommand};
 
 #[derive(Parser)]
 #[command(name = "monokkai")]
--- src/main.rs
+++ src/main.rs
@@ -3,23 +3,21 @@ mod core;
 mod modules;
 mod services;
 
-use crate::cli::{CliArgs, Commands};
 use clap::Parser;
+use std::error::Error;
 
 #[tokio::main]
-async fn main() {
-    let args = CliArgs::parse();
+async fn main() -> Result<(), Box<dyn Error>> {
+    let args = cli::CliArgs::parse();
 
     match args.command {
-        Commands::Git(args) => modules::git::git::handle(args),
-        Commands::Scan(args) => modules::docker::scan::handle(args),
-        Commands::Docker(args) => modules::docker::docker::handle(args),
-        Commands::Auth(args) => services::auth::handle(args),
-        Commands::Log(args) => modules::git::log::handle(&args),
-        Commands::Http(args) => {
-            if let Err(e) = modules::http::handler::handle(args.action).await {
-                eprintln!("Error: {}", e);
-            }
-        }
+        cli::Commands::Git(args) => modules::git::handle(args),
+        cli::Commands::Scan(args) => modules::docker::scan::handle(args),
+        cli::Commands::Docker(args) => modules::docker::handle(args),
+        cli::Commands::Auth(args) => services::auth::handle(args),
+        cli::Commands::Log(args) => modules::git::log::handle(&args),
+        cli::Commands::Http(args) => modules::http::handler::handle(args.action).await?,
     }
+
+    Ok(())
 }
